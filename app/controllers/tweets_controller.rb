b4141a927555b33ea00fb48ff851270b3708ce9f
yo2se2 <yo2se2study@gmail.com>
2020-11-22 04:06:36 +0900

    デザインを整える+ログイン機能をつけました。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,13 @@
 class TweetsController < ApplicationController
+    #authenticate_モデル名! 
+    #tweetsコントローラのページにアクセスしようとするユーザに対して，ログイン済みかどうかを確認してくれる。
+    before_action :authenticate_user!
+
+
     def index
         #テーブル名.new (オブジェクトの作成)
         @tweet = Tweet.new
+        @tweets = Tweet.all
     end
 
     def create
@@ -16,8 +22,34 @@ class TweetsController < ApplicationController
     def mypage
     end 
 
+    def show
+        #クラス変数に，送られてきたidのツイート情報を代入していく
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def update
+        tweet = Tweet.find(parms[:id])
+        if tweet.update(tweet_params)
+            redirect_to :action => "show", :id => tweet.id
+        else
+            redirect_to :action => "index"
+        end
+    end
+
+    def destroy
+        tweet = Tweet.find(params[:id])
+        #destroy  tweet変数の中に格納されているツイートに対応するレコードを削除してくれる。
+        tweet.destroy
+        redirect_to action: :index
+    end 
     private
     def tweet_params
-        params.require(:tweet).permit(:body)
+        #require(モデル名)でtweetモデルを指定
+        #permit(カラム名（キー)) 受け取る情報を絞る 
+        params.require(:tweet).permit(:body, :title)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,31 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  #モデルhelloのるーていんぐ
+  get 'hello/index' => 'hello#index'
 
   get 'tweets/home' => 'tweets#index'
+
   get 'tweets/mypage' => 'tweets#mypage'
   post 'tweets' => 'tweets#create'
 
-  root to: 'tweets#index' 
+  #ルーティングの設定 
+  # get (URLを実際のコードに割り振る。データを取得する処理に使う。)
+
+  # get "URLパターン"、=> "コントローラ名"#"アクション名"
+  # localhost:3000/tweets/homeにアクセスすると，indexメソッドを呼び出せるようになる。
+
+  # post (情報の新規追加のとき使用する。)
+  # データを変更したいときなどに使用（キャッシュしない） 
+
+  get 'tweets/:id' => 'tweets#show',as: 'tweet'
+  post 'tweets/:id' => 'tweets#show'
+
+  # editとupdateの追加
+  patch 'tweets/:id' => 'tweets#update'
+  delete 'tweets/:id' => 'tweets#destroy'
+  get 'tweets/:id/edit' => 'tweets#edit', as: 'edit_tweet'
+
+  root 'hello#index' 
 end
